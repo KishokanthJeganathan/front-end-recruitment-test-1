5f1d607fa05e69cbdfd70e43afca95e98ddde44d
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2021-01-29 22:56:58 +0100

    simple validation added on submit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,14 @@
-/* eslint-disable require-jsdoc */
-
 const moreBacon = () => {
 	const image = document.querySelector('#imageOfBacon');
-
-	// Create a copy of it
 	const clonedImage = image.cloneNode(true);
-
-	// Inject it into the DOM
 	image.after(clonedImage);
 };
+
+const logSubmit = (event) => {
+	log.textContent = `Wohoo! You just got yourself a great deal and an email confirming your payment should be in your inbox soon!`;
+	event.preventDefault();
+};
+
+const form = document.getElementById('form');
+const log = document.getElementById('log');
+form.addEventListener('submit', logSubmit);
